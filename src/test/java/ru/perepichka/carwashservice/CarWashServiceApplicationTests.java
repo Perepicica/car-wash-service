7feac672dfb58d1be46b5ae8e7bc80a3b169f5f5
Alexandra <sashika25@mail.ru>
2022-08-04 18:57:04 +0300

    initial commit

---
+++ src/test/java/ru/perepichka/carwashservice/CarWashServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.perepichka.carwashservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWashServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
